03b5ebd7a365a066ad8bf7de7f9f18d9e39cf961
Ashley Clarke <crashley92@gmail.com>
2019-12-14 10:29:42 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicky-game/precache-manifest.a1fb0196b31482516c0d07b8ca0dc579.js"
+  "/clicky-game/precache-manifest.84d9a79336bb97cb7dc1b4cfb5503b28.js"
 );
 
 self.addEventListener('message', (event) => {
